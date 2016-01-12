31b335832fbcdaafb5b25f02b172064c0604edae
Your Name <xingbupt@163.com>
2016-01-12 15:01:58 +0800

    add

--- func.cpp
+++ func.cpp
@@ -1,9 +1,9 @@
 #include "func.h"
 namespace commom{
-	void LOG_INFO(std::string& str){
+	void LOG_INFO(std::string str){
 		std::cout<<"LOG_INFO :"<<str<<std::endl;
 	}
-	void DEBUG_INFO(std::string& str){
+	void DEBUG_INFO(std::string str){
 		std::cout<<"DEBUG_INFO :"<<str<<std::endl;
 	}
 	std::string Func::GetLine( char* buffer ){
@@ -15,15 +15,15 @@ namespace commom{
 		std::string bufferstr = buffer;	
 		return bufferstr;	
 	}	
-	void  Func::Split(const char* d,const std::string& str, std::vector< std::string >& ret){
+	void  Func::Split(const char* d,const std::string& str, std::vector< std::string >& vec){
 		std::string  delim = d; 
-		ret.clear();
+		vec.clear();
 		size_t last = 0, index=str.find_first_of(delim,last);	
 		while (index!=std::string::npos){
-		ret.push_back(str.substr(last,index-last));	
+		vec.push_back(str.substr(last,index-last));	
 		last=index+1;index=str.find_first_of(delim,last);
 		}	
-		if (index-last>0)	ret.push_back(str.substr(last,index-last));
+		if (index-last>0)	vec.push_back(str.substr(last,index-last));
 	}
 	std::string  Func::Unit(const std::string& str){
 		std::string restr = "";
@@ -35,21 +35,21 @@ namespace commom{
 		}
 		return restr;
 	}
-	void  Func::SplitOnce(const char* d,const std::string& str, std::vector< std::string >& ret){
+	void  Func::SplitOnce(const char* d,const std::string& str, std::vector< std::string >& vec){
 		std::string delim = d;
-		ret.clear();
+		vec.clear();
 		int index = str.find(delim);
 		std::string temp ="";
 		temp.append(str, 0, index);
-		ret.push_back(temp);
+		vec.push_back(temp);
 		temp = "";
 		temp.append(str, index+1,str.size()-index );
-		ret.push_back(temp);
+		vec.push_back(temp);
 	}
 
-	void  Func::_Split(const char* d,const std::string& str, std::vector< std::string >& ret){
+	void  Func::_Split(const char* d,const std::string& str, std::vector< std::string >& vec){
 		std::string delim = d;
-		ret.clear();
+		vec.clear();
 		std::map<std::string, int>temp;
 		size_t last = 0, index=str.find_first_of(delim,last);
 		while (index!=std::string::npos){	
@@ -60,24 +60,67 @@ namespace commom{
 		if (index-last>0)	
 			temp[str.substr(last,index-last)]++;	
 		for(std::map<std::string, int>::iterator it = temp.begin(); it!= temp.end(); it++)
-			ret.push_back(it->first);
+			vec.push_back(it->first);
 	}
-	void  Func::Split(const char* d,const std::string& str, std::map<std::string, int>& ret){
+	void  Func::Split(const char* d,const std::string& str, std::map<std::string, int>& vec){
 		std::string delim = d;
-		ret.clear();
+		vec.clear();
 		size_t last = 0,index=str.find_first_of(delim,last);
 		while (index!=std::string::npos){	
-			ret[str.substr(last,index-last)]++;	
+			vec[str.substr(last,index-last)]++;	
 			last=index+1;
 			index=str.find_first_of(delim,last);
 		}	
-		if (index-last>0)	ret[str.substr(last,index-last)]++;	
+		if (index-last>0)	vec[str.substr(last,index-last)]++;	
 	}
-	template <class T> std::string Func::ConvertToStr(T value) {
-			std::stringstream ss;	
-			ss << value;	
-			return ss.str();
+	
+
+	bool Func::StrToVec(const std::string& str,  intvector& v){
+		char leftstr, rightstr;	
+		if(str.empty())return false;
+		v.clear();
+		for( unsigned int i = 0; i <str.size(); ){
+			if(!(str[i] & 0x80)) {
+				v.push_back(str[i++]);
+			}	else if ((unsigned char)str[i] <= 0xdf && i + 1 < str.size()) {
+				leftstr = (str[i] >> 2) & 0x07;	
+				rightstr = (str[i+1] & 0x3f) | ((str[i] & 0x03) << 6 );	
+				v.push_back(Combine(leftstr, rightstr));	i += 2;
+			}	else if ((unsigned char)str[i] <= 0xef && i + 2 < str.size()) {	
+				leftstr = (str[i] << 4) | ((str[i+1] >> 2) & 0x0f );	
+				rightstr = ((str[i+1]<<6) & 0xc0) | (str[i+2] & 0x3f); 
+				v.push_back(Combine(leftstr, rightstr));	i += 3;	
+			} else if((unsigned char)str[i] <= 0xf7 ){
+				i += 4;
+			} else if((unsigned char)str[i] <= 0xfb ){
+				i += 5;
+			} else if((unsigned char)str[i] <= 0xfd ){
+				i += 6;
+			}else{
+				return false;
+			}
+		}	
+		return true;
+}		
+	std::string Func::VecToStr(intvector& v, int i,int j ){
+		std::string str = "";	
+		if(i >= j) return str;	
+		while( i != j )	{	
+			if( v[i] <= 0x7f )	str += char( v[i]);	
+			else if( v[i] <= 0x7ff ){
+				str += char((( v[i]>>6) & 0x1f) | 0xc0);	
+				str += char(( v[i] & 0x3f) | 0x80);
+			} else{
+				str += char((( v[i] >> 12) & 0x0f )| 0xe0);
+				str += char((( v[i]>>6) & 0x3f )| 0x80 );	
+				str += char(( v[i] & 0x3f) | 0x80);
+			}	
+			i++;
+		}
+		return str;
 	}
+
+
 
 	char* Func::ReadLine( char* buffer, int  size, FILE *fi ){
 		memset( buffer,0,size );
--- func.h
+++ func.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include "string.h"
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/types.h>    
@@ -14,25 +15,31 @@
 #define SHORT_LENTH 250
 #define MAX_LENTH 50000
 typedef struct{ std::string word; 	float d;}wdlen;
+typedef  std::vector<uint16_t>  intvector;
 namespace commom{
 	//日志类
-	void LOG_INFO(std::string& str);
-	void DEBUG_INFO(std::string& str);
+	void LOG_INFO(std::string str);
+	void DEBUG_INFO(std::string str);
 	class Func{
-	public:
+	public://字符串处理
 		std::string GetLine( char* buffer );
 		std::string  Unit(const std::string& str);
-		void Split(const char* d,const std::string& str, std::vector< std::string >& ret);
-		void Split(const char* d,const std::string& str, std::map<std::string, int>& ret);
-		void _Split(const char* d,const std::string& str, std::vector< std::string >& ret);
-		void SplitOnce(const char* d,const std::string& str, std::vector< std::string >& ret);		
-		template <class T> std::string ConvertToStr(T value);
-	public:
+		void Split(const char* d,const std::string& str, std::vector< std::string >& vec);
+		void Split(const char* d,const std::string& str, std::map<std::string, int>& vec);
+		void _Split(const char* d,const std::string& str, std::vector< std::string >& vec);
+		void SplitOnce(const char* d,const std::string& str, std::vector< std::string >& vec);			
+		inline uint16_t Combine(char high, char low){	return (((uint16_t(high) & 0x00ff ) << 8) | (uint16_t(low) & 0x00ff));	}		
+		bool StrToVec(const std::string& str,  intvector& v);
+		std::string  VecToStr(intvector& v, int i,int j );
+	public://文件处理
 		char* ReadLine( char* buffer, int  size, FILE *fi );
 		int WiteLine( const char *str, FILE *fo );
-	public:
+	public://常用模版
 		template <class T> int MaxArray(T* prob, int n);
 		template <class T> int MinArray(T* prob, int n);
+		template <class T> std::string ConvertToStr(T value) {
+			std::stringstream ss;	ss << value;	return ss.str();
+		}
 		static bool SortByDistance(const wdlen& x, const wdlen& y){
 			return x.d > y.d;
 		}
